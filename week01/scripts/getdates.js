1437b63c7da3bd8ede7fdf61f2092a0dd0c2ddbc
pkc2610 <phoebekayc@gmail.com>
2024-09-28 13:02:54 -0700

    I'm learning things!!

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -4,3 +4,8 @@ console.log(today.getFullYear())
 
 const year = document.querySelector("#currentyear");
 year.innerHTML = today.getFullYear()
+
+console.log(document.lastModified)
+
+const lol = document.querySelector("#currentyear");
+lol.innerHTML = document.lastModified
